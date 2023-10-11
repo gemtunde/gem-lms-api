a86fb6e0badc5da1429970412ad6b831f0ef741d
gemtunde <gemtunde@gmail.com>
2023-10-11 17:43:09 +0100

    notification and order model

---
+++ models/notificationModel.ts
@@ -0,0 +1,38 @@
+import mongoose, { Document, Schema, Model } from "mongoose";
+
+export interface INotification extends Document {
+  title: string;
+  message: string;
+  status: string;
+  userId: string;
+}
+
+const notificationSchema = new Schema<INotification>(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Notification: Model<INotification> = mongoose.model(
+  "notifications",
+  notificationSchema
+);
+export default Notification;
